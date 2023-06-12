72baafa45e6fa399d4bbfa43c08f0e3938639e3a
airat <c098vm@ya.ru>
2023-06-12 17:35:22 +0500

    homework_6

--- src/video.py
+++ src/video.py
@@ -9,16 +9,22 @@ class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id  # id видео
-        self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                          id=video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']  # название видео
-        self.url: str = f'https://www.youtube.com/watch?v={video_id}'  # url-адрес видео
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']  # количество просмотров
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']  # количество лайков
+        try:
+            self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                              id=video_id).execute()
+            self.title: str = self.video_response['items'][0]['snippet']['title']  # название видео
+            self.url: str = f'https://www.youtube.com/watch?v={video_id}'  # url-адрес видео
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']  # количество просмотров
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']  # количество лайков
+        except Exception:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         """ Функция для отображения информации об  объекте для пользователей"""
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
